524760d6473b9714240551f32022e216df8df048
Szwarc Adam <adam.szwarc@nokia.com>
2017-06-28 22:08:25 +0200

    Function to show geolocation on page

--- js/local_weather.js
+++ js/local_weather.js
@@ -1,12 +1,15 @@
+function printPosition(latitude, longitude) {
+  document.getElementById("latitude").innerHTML = "Latitude: " + latitude;
+  document.getElementById("longitude").innerHTML = "Longitude: " + longitude;
+}
+
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      console.log(position.coords.latitude);
-      console.log(position.coords.longitude);
+      printPosition(position.coords.latitude, position.coords.longitude);
     });
   }
   else {
     console.log("Couldn't get geolocation");
   }
-
-  }
+}
