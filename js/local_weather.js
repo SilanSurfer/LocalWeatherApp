ff340078b11c98cdbaaa6e467a1fb58a2b784e05
Szwarc Adam <adam.szwarc@nokia.com>
2017-06-28 22:30:51 +0200

    Added function to handle reverse geocoding. Address extraction needs to be done.

--- js/local_weather.js
+++ js/local_weather.js
@@ -3,10 +3,29 @@ function printPosition(latitude, longitude) {
   document.getElementById("longitude").innerHTML = "Longitude: " + longitude;
 }
 
+function reverseGeocoding(latitude, longitude) {
+  $.ajax({
+    url : "https://maps.googleapis.com/maps/api/geocode/json?",
+    data : {
+      latlng : latitude + "," + longitude,
+      key : "AIzaSyCJLV016miHea8D-San-3o_n5y2d0KculI"
+    },
+    success : function(json) {
+      console.log(JSON.stringify(json));
+    },
+    error : function(status) {
+      console.log(status);
+    }
+  });
+}
+
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      printPosition(position.coords.latitude, position.coords.longitude);
+      var latitude = position.coords.latitude;
+      var longitude = position.coords.longitude;
+      printPosition(latitude, longitude);
+      reverseGeocoding(latitude, longitude);
     });
   }
   else {
