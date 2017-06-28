a73b6bfb0ee2e25242d0910f7cedc8d30e813c9b
Szwarc Adam <adam.szwarc@nokia.com>
2017-06-29 00:43:45 +0200

    Getting weather forecast data and displaying it on the page.

--- js/local_weather.js
+++ js/local_weather.js
@@ -4,15 +4,16 @@ function printPosition(latitude, longitude) {
 }
 
 function getAddressFromResponse(json) {
-  return JSON.stringify(json.results[0].formatted_address);
+  return json.results[0].formatted_address;
 }
 
-function reverseGeocoding(latitude, longitude) {
+function getAddressFromPosition(latitude, longitude, lang = "en") {
   $.ajax({
     url : "https://maps.googleapis.com/maps/api/geocode/json?",
     data : {
       latlng : latitude + "," + longitude,
-      key : "AIzaSyCJLV016miHea8D-San-3o_n5y2d0KculI"
+      key : "AIzaSyCJLV016miHea8D-San-3o_n5y2d0KculI",
+      language : lang
     },
     success : function(json) {
       document.getElementById("address").innerHTML = getAddressFromResponse(json);
@@ -24,13 +25,45 @@ function reverseGeocoding(latitude, longitude) {
   });
 }
 
+function getAndInsertWeatherData(json) {
+  document.getElementById("summary").innerHTML = json.currently.summary;
+  document.getElementById("current_temp").innerHTML = json.currently.temperature;
+  document.getElementById("icon").innerHTML = json.currently.icon;
+
+}
+
+function getWeatherForecast(latitude, longitude, units = "us", lang = "en") {
+  if(units !== "us" && units !== "si") {
+    units = "us";
+  }
+  if (lang !== "en" && lang !== "pl") {
+    lang = "en";
+  }
+  var requestedUnits = "units=" + units;
+  var language = "lang=" + lang;
+  var key = "e927a55a30254634ab211512c50acda7";
+  var exclude = "exclude=minutely,hourly,daily,alerts";
+  $.get("https://api.darksky.net/forecast/" + 
+          key + "/" + 
+          latitude + "," + longitude + 
+          "?" + exclude +
+          "&" + requestedUnits +
+          "&" + language,
+    function(data) {
+      console.log(JSON.stringify(data));
+      getAndInsertWeatherData(data);
+    },
+    "jsonp");
+}
+
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       var latitude = position.coords.latitude;
       var longitude = position.coords.longitude;
       printPosition(latitude, longitude);
-      reverseGeocoding(latitude, longitude);
+      getAddressFromPosition(latitude, longitude, "pl");
+      getWeatherForecast(latitude, longitude, "si", "pl");
     });
   }
   else {
